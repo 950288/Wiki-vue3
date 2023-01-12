fbdf334f8d77277580d4c9b3b9162d625a9a2f41
950288 <950288s@gmail.com>
2023-01-12 15:59:53 +0800

    uploads

---
+++ src/components/Tablegotool.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tableview">
+      <table class="table" id="table">
+        <slot />
+      </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.tableview {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+  .content {
+    background: #fff;
+    width: 60%;
+
+    #table {
+      width: 100%;
+
+      th {
+      }
+    }
+  }
+}
+</style>
--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="content is-medium">
-    <!-- iGEMGotool contact1 start-->
-    <h1>Hello World</h1>
-    <ol type="1">
+    <!-- iGEMGotool contact1 start--><table-pro><h1>Basic part</h1><Tablegotool><tr><th colspan="1" rowspan="1"><h1>Part Number</h1></th><th colspan="1" rowspan="1"><h1>Name</h1></th><th colspan="1" rowspan="1"><h1>length</h1></th><th colspan="1" rowspan="1"><h1>Type</h1></th></tr><tr><td colspan="1" rowspan="1"><h1><a target="_blank" rel="noopener noreferrer nofollow" href="http://parts.igem.org/Part:BBa_K4227000">K4227000</a></h1></td><td colspan="1" rowspan="1"><h1>p7z6</h1></td><td colspan="1" rowspan="1"><h1>3193 bp</h1></td><td colspan="1" rowspan="1"><h1>plasmid</h1></td></tr><tr><td colspan="1" rowspan="1"><h1><a target="_blank" rel="noopener noreferrer nofollow" href="http://parts.igem.org/Part:BBa_K4227001">K4227001</a></h1></td><td colspan="1" rowspan="1"><h1>MuB</h1></td><td colspan="1" rowspan="1"><h1>3882 bp</h1></td><td colspan="1" rowspan="1"><h1>coding</h1></td></tr><tr><td colspan="1" rowspan="1"><h1><a target="_blank" rel="noopener noreferrer nofollow" href="http://parts.igem.org/Part:BBa_K4227002">K4227002</a></h1></td><td colspan="1" rowspan="1"><h1>Monellin</h1></td><td colspan="1" rowspan="1"><h1>294 bp</h1></td><td colspan="1" rowspan="1"><h1>coding</h1></td></tr><tr><td colspan="1" rowspan="1"><h1><a target="_blank" rel="noopener noreferrer nofollow" href="http://parts.igem.org/Part:BBa_K4227003">K4227003</a></h1></td><td colspan="1" rowspan="1"><h1>xip-TP</h1></td><td colspan="1" rowspan="1"><h1>87 bp</h1></td><td colspan="1" rowspan="1"><h1>coding</h1></td></tr><tr><td colspan="1" rowspan="1"><h1><a target="_blank" rel="noopener noreferrer nofollow" href="http://parts.igem.org/Part:BBa_K4227005">K4227005</a></h1></td><td colspan="1" rowspan="1"><h1>xip-P34</h1></td><td colspan="1" rowspan="1"><h1>27 bp</h1></td><td colspan="1" rowspan="1"><h1>coding</h1></td></tr><tr><td colspan="1" rowspan="1"><h1><a target="_blank" rel="noopener noreferrer nofollow" href="http://parts.igem.org/Part:BBa_K4227006">K4227006</a></h1></td><td colspan="1" rowspan="1"><h1>P<sub>nlmC</sub>-site+</h1></td><td colspan="1" rowspan="1"><h1>154 bp</h1></td><td colspan="1" rowspan="1"><h1>regulatory</h1></td></tr><tr><td colspan="1" rowspan="1"><h1><a target="_blank" rel="noopener noreferrer nofollow" href="http://parts.igem.org/Part:BBa_K4227007">K4227007</a></h1></td><td colspan="1" rowspan="1"><h1>P<sub>nlmC</sub>-site-</h1></td><td colspan="1" rowspan="1"><h1>108 bp</h1></td><td colspan="1" rowspan="1"><h1>regulatory</h1></td></tr></Tablegotool></table-pro><h1></h1><!-- iGEMGotool contact1 end-->    <ol type="1">
       <li>Coffee</li>
       <li>Tea</li>
       <li>Milk</li>
@@ -51,4 +49,7 @@
     </ul>
   </div>
 </template>
+<script setup lang="ts">
+import Tablegotool from "@/components/Tablegotool.vue";
+</script>
 <style lang="scss" scoped></style>
