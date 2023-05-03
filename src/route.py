3386a49f0290442f6d927d7f36c4b06f5c4153ba
950288 <950288s@gmail.com>
2023-05-03 11:42:14 +0800

    use py to generate routes.ts

---
+++ src/route.py
@@ -0,0 +1,37 @@
+import json
+
+def generate_route_ts(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+
+    routes = data['routes']
+
+    imports = []
+    components = []
+
+    for route in routes:
+        component_name = route['name']
+        component_file = f"@/pages/{component_name.lower()}.vue"
+        imports.append(f'import {component_name} from "{component_file}";')
+        components.append({
+            "component": component_name,
+            "name": route['name'],
+            "path": route['path']
+        })
+
+    imports_str = '\n'.join(imports)
+    components_str = ',\n        '.join([str(component) for component in components])
+
+    route_ts = f'''{imports_str}
+
+export default {{
+    "routes": [
+        {components_str}
+    ]
+}}
+'''
+
+    with open('routes.ts', 'w') as f:
+        f.write(route_ts)
+
+generate_route_ts('routes.json')
---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import Home from "@/pages/home.vue";
+import About from "@/pages/about.vue";
+import Contact from "@/pages/contact.vue";
+import Look from "@/pages/look.vue";
+import Test from "@/pages/test.vue";
+import Editor from "@/pages/editor.vue";
+
+export default {
+    "routes": [
+        {'component': 'Home', 'name': 'Home', 'path': '/'},
+        {'component': 'About', 'name': 'About', 'path': '/about'},
+        {'component': 'Contact', 'name': 'Contact', 'path': '/contact'},
+        {'component': 'Look', 'name': 'Look', 'path': '/look'},
+        {'component': 'Test', 'name': 'Test', 'path': '/test'},
+        {'component': 'Editor', 'name': 'Editor', 'path': '/editor'}
+    ]
+}
