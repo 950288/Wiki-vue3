9669398b7c22cbde176c91654bf24cf18988c598
950288 <950288s@gmail.com>
2023-06-21 23:03:51 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,12 @@
     <div class="detect col-md-0" ref="detectInMD">
       1
     </div>
-    <div class="col-22 col-lg-23 col-md-24 offset-md-0 row">
-      <SideBar class="col-md-0" :class="[{ 'col-1': !toggleSidebar }, { 'col-4': toggleSidebar }]" v-show="inMD"
+    <!-- <div class="main col-22 col-lg-23 col-md-24 offset-md-0 row"> -->
+    <div class="col-24 row">
+      <SideBar class="col-md-0" :class="[{ 'col-1': !toggleSidebar }, { 'col-3': toggleSidebar }]" v-show="inMD"
         @toggle-sidebar="toggleSidebar = toggleSidebar ? false : true" />
       <router-view :root_bg="root_bg" class="col-md-24"
-        :class="[{ 'col-22 offset-1': !toggleSidebar }, { 'col-20': toggleSidebar }]" />
+        :class="[{ 'col-20 offset-1 col-lg-21': !toggleSidebar }, { 'col-20': toggleSidebar }]" />
     </div>
   </div>
 </template>
